558eaaa13930cdfc7d24275687d912953de30dbc
李慎广 <lishenguang1@126.com>
2022-04-19 15:58:05 +0800

    fix: 修改文件夹

--- start.sh
+++ start.sh
@@ -1,19 +1,15 @@
 if [ "$(docker ps -q -f name=fe)" ]; then
-    echo 'not fe run'
+    docker stop fe    
     docker rm fe
     docker rmi testfe
-
-    # run your container
 fi
 if [ "$(docker ps -aq -f status=exited -f name=fe)" ]; then
-    # cleanup
-    echo 'docker stop '
     docker rm fe
     docker rmi testfe
 fi
 docker build -t testfe .
 docker run -itd --restart=always \
 -v /home/ubuntu/project/felogs:/var/log/nginx \
--v /home/ubuntu/project/felog/nginx:/etc/nginx/conf.d \
--v /home/ubuntu/project/felog/dist:/usr/share/nginx/html \
+-v /home/ubuntu/project/testfe/nginx:/etc/nginx/conf.d \
+-v /home/ubuntu/project/testfe/dist:/usr/share/nginx/html \
 --name fe  -p 8002:80 testfe
