d074120efc983dc65954ae0c8fb06add1cd677e7
李慎广 <lishenguang1@126.com>
2022-04-19 14:51:47 +0800

    fix: sh

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@ if [ ! "$(docker ps -q -f name=abcd)" ]; then
     if [ "$(docker ps -aq -f status=exited -f name=abcd)" ]; then
         # cleanup
         echo 'docker stop '
+        docker rmi testfe
         docker rm abcd
     fi
     docker build -t testfe .
