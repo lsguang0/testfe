ccc4ee5b44dd267b3b562b3bf03af01db09f05f6
李慎广 <lishenguang1@126.com>
2022-04-19 14:07:36 +0800

    feat: 增加sh

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,4 @@
 FROM nginx
-MAINTAINER testfe
-ADD my.conf /etc/nginx/conf.d
-ADD /home/ubuntu/project/felog/dist /usr/share/nginx/html/
+ADD ./my.conf /etc/nginx/conf.d
+ADD ./dist /usr/share/nginx/html/
 ADD /home/ubuntu/project/logs /var/log/nginx/
---
+++ start.sh
@@ -0,0 +1,11 @@
+docker build testfe .
+if [ ! "$(docker ps -q -f name=testfe)" ]; then
+    echo 'not testfe run'
+    if [ "$(docker ps -aq -f status=exited -f name=testfe)" ]; then
+        # cleanup
+        echo 'docker stop '
+        docker rm testfe
+    fi
+    # run your container
+    docker run -itd --name testfe -p 8002:80 -v /home/ubuntu/project/felog/my.conf:/etc/nginx/conf.d -v /home/ubuntu/project/felog/dist:/usr/share/nginx/html/ testfe
+fi
