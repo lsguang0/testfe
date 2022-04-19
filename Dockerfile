830c91509470634fea301a0f532fe990d6bc39f1
李慎广 <lishenguang1@126.com>
2022-04-19 14:20:45 +0800

    fix: sh

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,3 @@
 FROM nginx
 ADD ./my.conf /etc/nginx/conf.d
 ADD ./dist /usr/share/nginx/html/
-ADD /home/ubuntu/project/logs /var/log/nginx/
--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
-docker build -t testfe .
 if [ ! "$(docker ps -q -f name=testfe)" ]; then
     echo 'not testfe run'
     if [ "$(docker ps -aq -f status=exited -f name=testfe)" ]; then
@@ -6,6 +5,7 @@ if [ ! "$(docker ps -q -f name=testfe)" ]; then
         echo 'docker stop '
         docker rm testfe
     fi
+    docker build -t testfe --no-cache.
     # run your container
     docker run -itd --name testfe -p 8002:80 -v /home/ubuntu/project/felog/my.conf:/etc/nginx/conf.d -v /home/ubuntu/project/felog/dist:/usr/share/nginx/html/ testfe
 fi
