bd3e3cacf52f33b89baffb459607623df74d5906
李慎广 <lishenguang1@126.com>
2022-04-19 14:26:46 +0800

    fix: sh

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
-if [ ! "$(docker ps -q -f name=testfe)" ]; then
+if [ ! "$(docker ps -q -f name=abcd)" ]; then
     echo 'not testfe run'
-    if [ "$(docker ps -aq -f status=exited -f name=testfe)" ]; then
+    if [ "$(docker ps -aq -f status=exited -f name=abcd)" ]; then
         # cleanup
         echo 'docker stop '
         docker rm testfe
     fi
     docker build -t testfe --no-cache .
     # run your container
-    docker run -itd --name testfe -p 8002:80 -v /home/ubuntu/project/felog/my.conf:/etc/nginx/conf.d -v /home/ubuntu/project/felog/dist:/usr/share/nginx/html/ testfe
+    docker run -itd --name abcd -p 8002:80 -v /home/ubuntu/project/felog/my.conf:/etc/nginx/conf.d -v /home/ubuntu/project/felog/dist:/usr/share/nginx/html/ testfe
 fi
