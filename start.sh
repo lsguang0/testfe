4cc2a462f627cbe1eeeeef1c19822437a77a4798
李慎广 <lishenguang1@126.com>
2022-04-19 14:54:21 +0800

    fix: shsh

--- start.sh
+++ start.sh
@@ -3,9 +3,9 @@ if [ ! "$(docker ps -q -f name=abcd)" ]; then
     if [ "$(docker ps -aq -f status=exited -f name=abcd)" ]; then
         # cleanup
         echo 'docker stop '
-        docker rmi testfe
         docker rm abcd
     fi
+    docker rmi testfe
     docker build -t testfe .
     # run your container
     docker run -itd --name abcd -p 8002:80 testfe
